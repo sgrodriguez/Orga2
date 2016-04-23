23af1fc74690c988e724a95f9bf282bd0038fc0f
Matias <matimatote@gmail.com>
2016-04-23 13:26:15 -0300

    Por hacerlo rapido me la re mande. Ahi lo arregle y lo puse en el primer loop

--- TP2/codigo/filtros/sepia_c.c
+++ TP2/codigo/filtros/sepia_c.c
@@ -13,16 +13,6 @@ void sepia_c    (
     unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
     unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
 
-    for (int i = 0; i < filas; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            bgra_t *p_d = (bgra_t*) &dst_matrix[i][j * 4];
-            bgra_t *p_s = (bgra_t*) &src_matrix[i][j * 4];
-            *p_d = *p_s;
-        }
-    }	//COMPLETAR
-
     unsigned short suma;
 
     for (int i = 0; i < filas; i++)
@@ -30,12 +20,27 @@ void sepia_c    (
         for (int j = 0; j < cols; j++)
         {
             bgra_t *p_d = (bgra_t*) &dst_matrix[i][j * 4];
-            suma = bgra_t->r + bgra_t->g + bgra_t->b
-            bgra_t->r = (unsigned char) suma * 0.5
-            bgra_t->g = (unsigned char) suma * 0.3
-            bgra_t->b = (unsigned char) suma * 0.2
+            bgra_t *p_s = (bgra_t*) &src_matrix[i][j * 4];
+            //*p_d = *p_s;
+            suma = p_s->r + p_s->g + p_s->b
+            p_d->r = (unsigned char) suma * 0.5
+            p_d->g = (unsigned char) suma * 0.3
+            p_d->b = (unsigned char) suma * 0.2
+            p_d->a = p_s->a
         }
-    }
+    }	//COMPLETAR
+
+    /*for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            bgra_t *p_d = (bgra_t*) &dst_matrix[i][j * 4];
+            suma = p_d->r + p_d->g + p_d->b
+            p_d->r = (unsigned char) suma * 0.5
+            p_d->g = (unsigned char) suma * 0.3
+            p_d->b = (unsigned char) suma * 0.2
+        }
+    }*/
 }
 
 
