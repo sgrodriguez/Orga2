16e6a55fdeed2f45fac7433ee2ea1ed09177689c
Matias <matimatote@gmail.com>
2016-04-24 18:50:25 -0300

    Cambie un poco los comentarios

--- TP2/codigo/filtros/sepia_asm.asm
+++ TP2/codigo/filtros/sepia_asm.asm
@@ -31,13 +31,16 @@ sepia_asm:
 		mov EAX, EDX
 		.ciclo_cols:
 			;En R11 tengo R10 (el offset de la fila en la que estoy parado) + 16 * [cantidad de paquetes de pixeles procesados]
-			;Hacer cosas con el paquete
+			
+			;Traigo el paquete de pixeles a XMM0 y XMM1
 			movdqu XMM0, [RDI + R11] 	; XMMO = [ a3 | r3 | g3 | b3 || a2 | r2 | g2 | b2 || a1 | r1 | g1 | b1 || a0 | r0 | g0 | b0 ]
 			movdqu XMM1, XMM0
+
+			;Aumento precision de Byte a Word para todos canales de los pixeles (sino no puedo hacer la suma de RGB sin asegurarme que no haya OVERFLOW)
 			punpcklbw XMM1, XMM14		; XMM1 = [ a1 | r1 | g1 | b1 || a0 | r0 | g0 | b0 ]
 			punpckhbw XMM0, XMM14		; XMM0 = [ a3 | r3 | g3 | b3 || a2 | r2 | g2 | b2 ]
 			
-			;Trabajo con XMM1 (los dos primeros pixeles)
+			;Sumo los valores de R, G y B de los 2 pixeles de XMM1, y distrubuyo esa suma a todos los canales (excepto alpha)
 			movdqu XMM2, XMM1			; XMM2 = [ a1 | r1 | g1 | b1 || a0 | r0 | g0 | b0 ]
 			psrlq XMM2, 16				; XMM2 = [  0 | a1 | r1 | g1 ||  0 | a0 | r0 | g0 ]
 			paddw XMM1, XMM2			; XMM1 = [ a1 |  - |  - | b1 + g1 || a0 |  - |  - | b0 + g0 ]
@@ -46,7 +49,7 @@ sepia_asm:
 			pshuflw XMM1, XMM1, 0xC0	; XMM1 = [ a1 |  - |  - | b1 + g1 + r1 || a0 | s0 | s0 | s0 ]
 			pshufhw XMM1, XMM1, 0xC0	; XMM1 = [ a1 | s1 | s1 | s1 || a0 | s0 | s0 | s0 ]
 
-			;Metodo 1, conversion a float, multiplicacion por float y conversion a entero nuevamente
+			;Metodo 1, convierto XMM1 de Word a D-Word, y de D-Word a Float de precision simple, realizo la multiplicacion, y vuelvo a Word
 			movdqu XMM2, XMM1
 			punpcklwd XMM1, XMM14		; XMM1 = [ a0 | s0 | s0 | s0 ]
 			punpckhwd XMM2, XMM14		; XMM2 = [ a1 | s1 | s1 | s1 ]
@@ -58,7 +61,7 @@ sepia_asm:
 			cvtps2dq XMM1, XMM3			; XMM1 = [ a0 * 1.0 | s0 * 0.5 | s0 * 0.3 | s0 * 0.2 ] ** Le agrego un _s a los canales ya procesados
 			packusdw XMM1, XMM2			; XMM1 = [ a1_s | r1_s | g1_s | b1_s || a0_s | r0_s | g0_s | b0_s ]
 
-			;Trabajo con XMM0 (los dos ultimos pixeles)
+			;Sumo los valores de R, G y B de los 2 pixeles de XMM0, y distrubuyo esa suma a todos los canales (excepto alpha)
 			movdqu XMM2, XMM0			; XMM2 = [ a3 | r3 | g3 | b3 || a2 | r2 | g2 | b2 ]
 			psrlq XMM2, 16				; XMM2 = [  0 | a3 | r3 | g3 ||  0 | a2 | r2 | g2 ]
 			paddw XMM0, XMM2			; XMM0 = [ a3 |  - |  - | b3 + g3 || a2 |  - |  - | b2 + g2 ]
@@ -67,7 +70,7 @@ sepia_asm:
 			pshuflw XMM0, XMM0, 0xC0	; XMM0 = [ a3 |  - |  - | b3 + g3 + r3 || a2 | s2 | s2 | s2 ]
 			pshufhw XMM0, XMM0, 0xC0	; XMM0 = [ a3 | s3 | s3 | s3 || a2 | s2 | s2 | s2 ]
 
-			;Metodo 1, conversion a float, multiplicacion por float y conversion a entero nuevamente
+			;Metodo 1, convierto XMM2 de Word a D-Word, y de D-Word a Float de precision simple, realizo la multiplicacion, y vuelvo a Word
 			movdqu XMM2, XMM0			
 			punpcklwd XMM0, XMM14		; XMM0 = [ a2 | s2 | s2 | s2 ]
 			punpckhwd XMM2, XMM14		; XMM2 = [ a3 | s3 | s3 | s3 ]
@@ -79,7 +82,7 @@ sepia_asm:
 			cvtps2dq XMM0, XMM3			; XMM0 = [ a2 * 1.0 | s2 * 0.5 | s2 * 0.3 | s2 * 0.2 ] ** Le agrego un _s a los canales ya procesados
 			packusdw XMM0, XMM2			; XMM0 = [ a3_s | r3_s | g3_s | b3_s || a2_s | r2_s | g2_s | b2_s ]
 
-			;Re-empaquetado
+			;Re-empaqueto de Word a Byte con saturacion
 			packuswb XMM1, XMM0			; XMM1 = [ a3_s | r3_s | g3_s | b3_s || a2_s | r2_s | g2_s | b2_s || a1_s | r1_s | g1_s | b1_s || a0_s | r0_s | g0_s | b0_s ]
 			movdqu [RSI + R11], XMM1	;Guardo en memoria
 
