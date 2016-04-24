772867429694a40bec5ce5f9da24d1f06041ab70
Matias <matimatote@gmail.com>
2016-04-24 17:44:35 -0300

    Termine de agregar los comentarios que explican el estado de los registros

--- TP2/codigo/filtros/sepia_asm.asm
+++ TP2/codigo/filtros/sepia_asm.asm
@@ -50,38 +50,38 @@ sepia_asm:
 			movdqu XMM2, XMM1
 			punpcklwd XMM1, XMM14		; XMM1 = [ a0 | s0 | s0 | s0 ]
 			punpckhwd XMM2, XMM14		; XMM2 = [ a1 | s1 | s1 | s1 ]
-			cvtdq2ps XMM3, XMM1			; XMM3 = [ a0 | s0 | s0 | s0 ]
-			cvtdq2ps XMM4, XMM2			; XMM4 = [ a1 | s1 | s1 | s1 ]
-			mulps XMM4, XMM15
-			mulps XMM3, XMM15
-			cvtps2dq XMM2, XMM4
-			cvtps2dq XMM1, XMM3
-			packusdw XMM1, XMM2
+			cvtdq2ps XMM3, XMM1			; XMM3 = [ a0 | s0 | s0 | s0 ] * floats
+			cvtdq2ps XMM4, XMM2			; XMM4 = [ a1 | s1 | s1 | s1 ] * floats
+			mulps XMM4, XMM15			; XMM4 = [ a1 * 1.0 | s1 * 0.5 | s1 * 0.3 | s1 * 0.2 ] * floats
+			mulps XMM3, XMM15			; XMM3 = [ a0 * 1.0 | s0 * 0.5 | s0 * 0.3 | s0 * 0.2 ] * floats
+			cvtps2dq XMM2, XMM4			; XMM2 = [ a1 * 1.0 | s1 * 0.5 | s1 * 0.3 | s1 * 0.2 ]
+			cvtps2dq XMM1, XMM3			; XMM1 = [ a0 * 1.0 | s0 * 0.5 | s0 * 0.3 | s0 * 0.2 ] ** Le agrego un _s a los canales ya procesados
+			packusdw XMM1, XMM2			; XMM1 = [ a1_s | r1_s | g1_s | b1_s || a0_s | r0_s | g0_s | b0_s ]
 
 			;Trabajo con XMM0 (los dos ultimos pixeles)
-			movdqu XMM2, XMM0			; XMM2 = [ a1 | r1 | g1 | b1 || a0 | r0 | g0 | b0 ]
-			psrlq XMM2, 16				; XMM2 = [  0 | a1 | r1 | g1 ||  0 | a0 | r0 | g0 ]
-			paddw XMM0, XMM2			; XMM0 = [ a1 |  - |  - | b1 + g1 || a0 |  - |  - | b0 + g0 ]
-			psrlq XMM2, 16				; XMM2 = [  0 |  0 | a1 | r1 ||  0 |  0 | a0 | r0 ]
-			paddw XMM0, XMM2			; XMM0 = [ a1 |  - |  - | b1 + g1 + r1 || a0 |  - |  - | b0 + g0 + r0 ]	** bi + gi + ri = si
-			pshuflw XMM0, XMM0, 0xC0	; XMM0 = [ a1 |  - |  - | b1 + g1 + r1 || a0 | s0 | s0 | s0 ]
-			pshufhw XMM0, XMM0, 0xC0	; XMM0 = [ a1 | s1 | s1 | s1 || a0 | s0 | s0 | s0 ]
+			movdqu XMM2, XMM0			; XMM2 = [ a3 | r3 | g3 | b3 || a2 | r2 | g2 | b2 ]
+			psrlq XMM2, 16				; XMM2 = [  0 | a3 | r3 | g3 ||  0 | a2 | r2 | g2 ]
+			paddw XMM0, XMM2			; XMM0 = [ a3 |  - |  - | b3 + g3 || a2 |  - |  - | b2 + g2 ]
+			psrlq XMM2, 16				; XMM2 = [  0 |  0 | a3 | r3 ||  0 |  0 | a2 | r2 ]
+			paddw XMM0, XMM2			; XMM0 = [ a3 |  - |  - | b3 + g3 + r3 || a2 |  - |  - | b2 + g2 + r2 ]	** bi + gi + ri = si
+			pshuflw XMM0, XMM0, 0xC0	; XMM0 = [ a3 |  - |  - | b3 + g3 + r3 || a2 | s2 | s2 | s2 ]
+			pshufhw XMM0, XMM0, 0xC0	; XMM0 = [ a3 | s3 | s3 | s3 || a2 | s2 | s2 | s2 ]
 
 			;Metodo 1, conversion a float, multiplicacion por float y conversion a entero nuevamente
-			movdqu XMM2, XMM0
-			punpcklwd XMM0, XMM14		; XMM0 = [ a0 | s0 | s0 | s0 ]
-			punpckhwd XMM2, XMM14		; XMM2 = [ a1 | s1 | s1 | s1 ]
-			cvtdq2ps XMM3, XMM0			; XMM3 = [ a0 | s0 | s0 | s0 ]
-			cvtdq2ps XMM4, XMM2			; XMM4 = [ a1 | s1 | s1 | s1 ]
-			mulps XMM4, XMM15
-			mulps XMM3, XMM15
-			cvtps2dq XMM2, XMM4
-			cvtps2dq XMM0, XMM3
-			packusdw XMM0, XMM2
+			movdqu XMM2, XMM0			
+			punpcklwd XMM0, XMM14		; XMM0 = [ a2 | s2 | s2 | s2 ]
+			punpckhwd XMM2, XMM14		; XMM2 = [ a3 | s3 | s3 | s3 ]
+			cvtdq2ps XMM3, XMM0			; XMM3 = [ a2 | s2 | s2 | s2 ]
+			cvtdq2ps XMM4, XMM2			; XMM4 = [ a3 | s3 | s3 | s3 ]
+			mulps XMM4, XMM15			; XMM4 = [ a3 * 1.0 | s3 * 0.5 | s3 * 0.3 | s3 * 0.2 ] * floats
+			mulps XMM3, XMM15			; XMM3 = [ a2 * 1.0 | s2 * 0.5 | s2 * 0.3 | s2 * 0.2 ] * floats
+			cvtps2dq XMM2, XMM4			; XMM2 = [ a3 * 1.0 | s3 * 0.5 | s3 * 0.3 | s3 * 0.2 ]
+			cvtps2dq XMM0, XMM3			; XMM0 = [ a2 * 1.0 | s2 * 0.5 | s2 * 0.3 | s2 * 0.2 ] ** Le agrego un _s a los canales ya procesados
+			packusdw XMM0, XMM2			; XMM0 = [ a3_s | r3_s | g3_s | b3_s || a2_s | r2_s | g2_s | b2_s ]
 
 			;Re-empaquetado
-			packuswb XMM1, XMM0
-			movdqu [RSI + R11], XMM1
+			packuswb XMM1, XMM0			; XMM1 = [ a3_s | r3_s | g3_s | b3_s || a2_s | r2_s | g2_s | b2_s || a1_s | r1_s | g1_s | b1_s || a0_s | r0_s | g0_s | b0_s ]
+			movdqu [RSI + R11], XMM1	;Guardo en memoria
 
 
 		;Cuando termino de trabajar con el paquete de pixeles de la posicion i, me muevo a la posicion i + 4 (que esta 16 bytes mas adelante)
