a164566db95f2915e7301cd8bc12c5c662a95aa4
Santiago Gabriel Rodriguez <sgrodriguez@ws13.labo6>
2016-05-02 15:59:12 -0300

    mediorTiempo python de negro africano

--- TP2/codigo/experimentos/medirtiempo.py
+++ TP2/codigo/experimentos/medirtiempo.py
@@ -2,7 +2,7 @@
 from sys import argv
 import subprocess
 import time
-import statistics
+#import statistics
 import os
 
 #process = subprocess.Popen(["make clean; make;"], shell=True, stdout=subprocess.PIPE, cwd="../")
@@ -12,18 +12,24 @@ import os
 
 args = argv
 args[0] = "../build/tp2"
-
 f = open("outputFile","wb")
 
 caca = []
-
-for i in range(1000):
+iterar = int(args[-1])
+args.pop()
+for i in range(iterar):
 	start = time.time()
 	subprocess.call(args, stdout=f)
 	end = time.time()
 	#print( end-start )
 	caca.append(end-start)
 
-print ( statistics.median(caca) )
-
+#print ( statistics.median(caca) )
+caca.sort()
+iterar = iterar/2
+print(caca[iterar])#mediana
 os.remove("outputFile")
+
+#-v sepia -i asm ../tests/data/img/lena.1920x1080.bmp 100
+#-v ldr -i asm ../tests/data/img/lena.1920x1080.bmp 100 100
+#../build/tp2 -v ldr -i asm ../tests/data/img/lena.1920x1080.bmp 100
