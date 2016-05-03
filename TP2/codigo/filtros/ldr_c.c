4501d776ae4867427bbaf2b96879259e048f1fbe
Matias Lopez Luque <melopez@dc.uba.ar>
2016-05-02 23:08:16 -0300

    Modifique una cosita en ldr en C que hacia que no dieran los tests

--- TP2/codigo/filtros/ldr_c.c
+++ TP2/codigo/filtros/ldr_c.c
@@ -20,7 +20,7 @@ void ldr_c    (
 
     //Un unsigned short (2 bytes) alcanza para almacenar la sumaij porque el valor maximo es (0xFF * 25) = 0x18E7, que son 2 bytes
     unsigned short suma;
-    int ldr;
+    long long int ldr;
     int max = 5 * 5 * 255 * 3 * 255;
     bgra_t* p_temp;
 
@@ -32,8 +32,12 @@ void ldr_c    (
             bgra_t *p_s = (bgra_t*) &src_matrix[i][j * 4];
 
             //Si es un caso borde se mantiene el color del source
-            if (i < 2 || i > filas - 2 || j < 2 || j > cols - 2)
-                *p_d = *p_s;
+            if (i < 2 || i >= (filas - 2) || j < 2 || j >= (cols - 2)){
+                p_d->r = p_s->r;
+                p_d->g = p_s->g;
+                p_d->b = p_s->b;
+                p_d->a = p_s->a;
+            }
             else {
                 //Si no:
                 suma = 0;
@@ -46,10 +50,13 @@ void ldr_c    (
 
                 ldr = p_s->r + ((alpha * suma * p_s->r) / max);
                 p_d->r = (ldr < 0) ? 0 : ((ldr > 255) ? 255 : ldr);
+
                 ldr = p_s->g + ((alpha * suma * p_s->g) / max);
                 p_d->g = (ldr < 0) ? 0 : ((ldr > 255) ? 255 : ldr);
+
                 ldr = p_s->b + ((alpha * suma * p_s->b) / max);
                 p_d->b = (ldr < 0) ? 0 : ((ldr > 255) ? 255 : ldr);
+
                 p_d->a = p_s->a;
             }
 
